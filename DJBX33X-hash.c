a3695c767f305c6755bb9d12dfb2cc9abb5be846
lufeee <contact@lufe.jp>
2022-12-14 15:54:46 +0900

    DJBX's hash algo

---
+++ DJBX33X-hash.c
@@ -0,0 +1,33 @@
+
+
+// Daniel J. Berstein, Times 33 with OR
+
+static inline unsigned long djbx33a_hash_func(const char* arKey, nKeyLength) {
+  register unsigned long = 5381;
+
+  for (unsigned int i = 0; i < nkeylength; i++) {
+    hash = ((hash << 5) + hash) ^ arkey[i];
+  }
+
+  return hash;
+}
+
+// static inline unsigned long djbx33a_hash_func(const char* arKey, nKeyLength) {
+//   register unsigned long = 5381;
+// 
+//   for (unsigned int i = 0; i < nkeylength; i++) {
+//     hash = ((hash * 32) + hash) ^ arkey[i];
+//   }
+// 
+//   return hash;
+// }
+
+// static inline unsigned long djbx33a_hash_func(const char* arKey, nKeyLength) {
+//   register unsigned long = 5381;
+// 
+//   for (unsigned int i = 0; i < nkeylength; i++) {
+//     hash = (hash * 33) ^ arkey[i];
+//   }
+// 
+//   return hash;
+// }
